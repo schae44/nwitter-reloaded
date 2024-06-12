7df2660e09929da80baef1e0425b02f0da844484
Sarah <sarahchae0219@gmail.com>
2024-06-12 11:34:28 -0500

    (Redirection to login) Implemented

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ import reset from "styled-reset";
 import { useState, useEffect } from "react";
 import LoadingScreen from "./components/loading-screen";
 import { auth } from "./firebase";
+import ProtectedRoute from "./components/protected-route";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
     children: [
       {
         path: "",
